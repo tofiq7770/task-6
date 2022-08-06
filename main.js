3bbe3eac7d02f22a95c9809d381d428c030a311c
tofiq7770 <tofigtn@code.edu.az>
2022-08-06 08:54:22 +0400

    first commit

---
+++ main.js
@@ -0,0 +1,27 @@
+const numbers1 =[1,2,3,5];
+
+console.log(numbers1.length)
+function arrpush(){
+
+    for (let index = 0; index < numbers1.length; index++) {
+        let myArr = [];
+        myArr.push(index)
+        
+    }
+    console.log(numbers1.length)
+}
+
+
+let sum1 = 0;
+
+for (let i = 0; i < numbers1.length; i++) {
+    sum1 += numbers1[i];
+}
+let sum2 = 0;
+
+for (let i = 0; i < index.length; i++) {
+    sum2 += index[i];
+}
+console.log(sum1)
+console.log(sum2)
+
